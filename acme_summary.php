5fb0307c0911d75293686d47ed3c07962848695c
erjosito <erjosito@hotmail.com>
2015-11-22 22:24:18 +0100

    Renamed to acme_summary

--- acme_summary.php
+++ acme_summary.php
@@ -3,7 +3,7 @@
     <title>muc-server-01 generic web server</title>
   </header>
   <body>
-    <h1>EPG dashboard</h1>
+    <h1>ACME MyApp1 EPG dashboard</h1>
     <p>This page shows a summary of the End Points registered with each EPG</p>
 
     <?php
@@ -20,7 +20,7 @@ function newPie ($name, $title, $data1, $data2) {
         $graph->options->label = "";
         $graph->data['EPG count'] = new ezcGraphArrayDataSet( array(
            'EPs' => $data1,
-           '' => $data2,
+           '' => $data2 - $data1,
         ));
         $graph->render( 400, 150, "/var/www/html/" . $name . ".svg" );
 }
@@ -76,43 +76,100 @@ function getEPNumber ($token, $tenant, $anp, $epg) {
   $p = xml_parser_create();
   xml_parse_into_struct($p, $result, $vals, $index);
   xml_parser_free($p);
-  return $vals[0]['attributes']['TOTALCOUNT'];
 
-  //Close
-  ch_close ($ch);
+  // Get the individual IP addresses into an array
+  $my_ips = array ();
+  foreach ($index['FVIP'] as $ipindex) {
+    array_push ($my_ips, $vals[$ipindex]['attributes']['ADDR']);
+  }
 
+  // Return the array with the IPs
+  //return $vals[0]['attributes']['TOTALCOUNT'];
+  return $my_ips;
+}
+
+function printIPs ($ip_addresses, $width) {
+    // Print the EPs' IP addresses in a new table cell
+    print "<td width='" . $width . "'><ul>";
+    foreach ($ip_addresses as $ip) {
+        print "<li>" . $ip . "</li>";
+    }
+    print "</ul></td>";
 }
 
 //Max number of EPs in the pie diagrams
 $max = 10;
 
+// Initialize and start drawing table
 $token = apic_login();
-$max = 10;
-$epg1 = getEPNumber ($token, "Pod1", "Pod1", "EPG1");
-newPie ("pie1", "EPs in EPG 1", $epg1, $max);
-$epg2 = getEPNumber ($token, "Pod1", "Pod1", "EPG2");
-newPie ("pie2", "EPs in EPG 2", $epg2, $max);
+$max = 4;
+
+// PRD
+print "<h2>My App 1 - PRD</h2>";
+print "<center><table width='70%'><tr>";
+
+// First cell: Web-PRD
+$ips_epg1 = getEPNumber ($token, "Acme", "MyApp1-PRD", "Web");
+$epg1 = count ($ips_epg1);
+newPie ("pie1", "EPs in Web", $epg1, $max);
+print "<td width='50%'><img alt='Pie chart'  src='pie1.svg' style='border: 1px solid gray;'/></td>";
+
+$ips_epg2 = getEPNumber ($token, "Acme", "MyApp1-PRD", "DB");
+$epg2 = count ($ips_epg2);
+newPie ("pie2", "EPs in DB", $epg2, $max);
+print "<td width='50%'><img alt='Pie chart'  src='pie2.svg' style='border: 1px solid gray;'/></td></tr>";
+
+// Print the EPs' IP addresses in a new row
+print "<tr>";
+printIPs ($ips_epg1, "50%");
+printIPs ($ips_epg2, "50%");
+print "</tr>";
+
+// Finish the table
+print " </tr></table></center>";
+
+// TST
+print "<h2>My App 1 - TST</h2>";
+print "<center><table width='70%'><tr>";
+
+// First cell: Web-PRD
+$ips_epg3 = getEPNumber ($token, "Acme", "MyApp1-TST", "Web");
+$epg3 = count ($ips_epg3);
+newPie ("pie3", "EPs in Web", $epg3, $max);
+print "<td width='50%'><img alt='Pie chart'  src='pie3.svg' style='border: 1px solid gray;'/></td>";
+
+$ips_epg4 = getEPNumber ($token, "Acme", "MyApp1-TST", "DB");
+$epg4 = count ($ips_epg4);
+newPie ("pie4", "EPs in DB", $epg4, $max);
+print "<td width='50%'><img alt='Pie chart'  src='pie4.svg' style='border: 1px solid gray;'/></td></tr>";
+
+// Print the EPs' IP addresses in a new row
+print "<tr>";
+printIPs ($ips_epg3, "50%");
+printIPs ($ips_epg4, "50%");
+print "</tr>";
+
+// Finish the table
+print " </tr></table></center>";
 
 //Create bar graph
 $allEPGs = array (
-  'EPG1' => $epg1,
-  'EPG2' => $epg2,
+  'PRD-Web' => $epg1,
+  'PRD-DB' => $epg2,
+  'TST-Web' => $epg3,
+  'TST-DB' => $epg4,
 );
-newBar ("bar", "Summary", $allEPGs)  
+newBar ("bar", "Summary", $allEPGs);
 
-?>
-
-	    <table width='100%'>
-	      <tr>
-		  <td width='33%'><img alt='Pie chart'  src='pie1.svg' style='border: 1px solid gray;'/></td>
-		  <td width='33%'><img alt='Pie chart'  src='pie2.svg' style='border: 1px solid gray;'/></td>
-	      </tr>
-	    </table>
-
+// Draw bar graph
+print "<h2>My App 1 - Overall summary</h2>";
+print "
             <center>
               <p>Summary of EPG number across EPGs</p>
                 <img alt='Bar chart'  src='bar.svg' style='border: 1px solid gray;'/>
             </center>
+";
+?>
 
   </body>
 </html>
